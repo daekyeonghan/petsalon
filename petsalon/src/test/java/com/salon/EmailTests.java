e22f5a8e70bbd1686019d73472a43e3928cbc0dd
daekyeonghan <hdk1008@gmail.com>
2023-02-08 14:03:50 +0900

    HDK 20230208

--- petsalon/src/test/java/com/salon/EmailTests.java
+++ petsalon/src/test/java/com/salon/EmailTests.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Tests {
+class EmailTests {
 
 	@Test
 	void contextLoads() {
